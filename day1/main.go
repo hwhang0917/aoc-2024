c10d59e951625833a66401e7e4ea3dda6194293e
Heesnag Whang <hwhang0917@gmail.com>
2024-12-01 20:03:36 +0900

    [day_1] solved

--- day1/main.go
+++ day1/main.go
@@ -11,19 +11,38 @@ import (
 func main() {
     input := utils.ReadDayOneInput(false)
 
-    sort.Ints(input.LeftColumn)
-    sort.Ints(input.RightColumn)
+    // Part 1
+    sortedLeftColumn := make([]int, len(input.LeftColumn))
+    sortedRightColumn := make([]int, len(input.RightColumn))
+    copy(sortedLeftColumn, input.LeftColumn)
+    copy(sortedRightColumn, input.RightColumn)
+    sort.Ints(sortedLeftColumn)
+    sort.Ints(sortedRightColumn)
 
     distanceSum := 0
 
     for i := 0; i < input.Lines; i++ {
-        left := input.LeftColumn[i]
-        right := input.RightColumn[i]
+        left := sortedLeftColumn[i]
+        right := sortedRightColumn[i]
 
         distance := math.Abs(float64(left - right))
         distanceSum += int(distance)
     }
 
-    fmt.Println(distanceSum)
+    fmt.Printf("PART I solution: %d\n", distanceSum)
+
+    // Part 2
+    similarSum := 0
+    frequencyMap := make(map[int]int)
+    for _, num := range input.RightColumn {
+        frequencyMap[num]++
+    }
+    for i := 0; i < input.Lines; i++ {
+        left := input.LeftColumn[i]
+        similarity := frequencyMap[left]
+        similarSum += similarity * left
+    }
+
+    fmt.Printf("PART II solution: %d\n", similarSum)
 }
 
